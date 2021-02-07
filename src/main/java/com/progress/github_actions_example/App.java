4684edd690bb5d621fb070edc2ba2ac7bd5035fc
saiteja8848 <saiteja8848@gmail.com>
2021-02-07 16:58:20 +0530

    firstcommit

---
+++ src/main/java/com/progress/github_actions_example/App.java
@@ -0,0 +1,19 @@
+package com.progress.github_actions_example;
+
+class Calculator {
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public int sub(int a, int b) {
+		return a - b;
+	}
+
+}
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/com/progress/github_actions_example/AppTest.java
@@ -0,0 +1,24 @@
+package com.progress.github_actions_example;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AppTest {
+
+	private static Calculator calculator;
+
+	@BeforeClass
+	public static void setUp() {
+		calculator = new Calculator();
+	}
+
+	@Test
+	public void addTest() {
+		assertEquals(calculator.add(1, 1), 2);
+		assertEquals(calculator.add(0, -1), -1);
+		assertEquals(calculator.add(999, 1), 1000);
+		assertEquals(calculator.add(1, 100), 101);
+	}
+}
